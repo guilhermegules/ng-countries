041e6db7a2b64071643f5d3dd7eafa16943b7766
Guilherme Gules Moreira <guilhermegules@gmail.com>
2022-04-22 17:24:03 -0300

    feat: add search and filter for countries list

--- src/app/modules/country/containers/country-list/country-list.component.ts
+++ src/app/modules/country/containers/country-list/country-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { debounceTime, Observable, Subject, takeUntil } from 'rxjs';
 import { RegionEnum } from '../../enums/region.enum';
 
 import { Country } from '../../models/country.model';
@@ -10,18 +11,56 @@ import { CountryService } from '../../services/country.service';
   templateUrl: './country-list.component.html',
   styleUrls: ['./country-list.component.scss'],
 })
-export class CountryListComponent implements OnInit {
+export class CountryListComponent implements OnInit, OnDestroy {
   public countries$!: Observable<Country[]>;
   public regions: { value: string; label: string }[] = [];
+  public form!: FormGroup;
 
-  constructor(private countryService: CountryService) {}
+  private destroyed$ = new Subject<void>();
+
+  constructor(
+    private countryService: CountryService,
+    private fb: FormBuilder
+  ) {}
 
   public ngOnInit(): void {
+    this.initForm();
+
     this.countries$ = this.countryService.getAllCountries();
 
     this.regions = Object.values(RegionEnum).map((value) => ({
       value,
       label: value[0].toUpperCase() + value.slice(1),
     }));
+
+    this.formFieldListeners();
+  }
+
+  public ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
+
+  private initForm() {
+    this.form = this.fb.group({
+      search: null,
+      filter: null,
+    });
+  }
+
+  private formFieldListeners() {
+    this.form
+      .get('search')
+      ?.valueChanges.pipe(debounceTime(500), takeUntil(this.destroyed$))
+      .subscribe((search) => {
+        this.countries$ = this.countryService.getCountryByName(search);
+      });
+
+    this.form
+      .get('filter')
+      ?.valueChanges.pipe(debounceTime(500), takeUntil(this.destroyed$))
+      .subscribe((filter) => {
+        this.countries$ = this.countryService.getCountriesByContinent(filter);
+      });
   }
 }
--- src/app/modules/country/country.module.ts
+++ src/app/modules/country/country.module.ts
@@ -1,21 +1,19 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CountryRoutingModule } from './country-routing.module';
 import { CountryListComponent } from './containers/country-list/country-list.component';
 import { CountryDetailsComponent } from './containers/country-details/country-details.component';
 import { CountryCardComponent } from './components/country-card/country-card.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     CountryListComponent,
     CountryDetailsComponent,
-    CountryCardComponent
+    CountryCardComponent,
   ],
-  imports: [
-    CommonModule,
-    CountryRoutingModule
-  ]
+  imports: [CommonModule, CountryRoutingModule, ReactiveFormsModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class CountryModule { }
+export class CountryModule {}
