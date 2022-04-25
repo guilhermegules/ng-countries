140fbb940781429141372d76d57c4c6ca68aeb77
Guilherme Gules Moreira <guilhermegules@gmail.com>
2022-04-24 22:28:26 -0300

    feat(country-details): creating details view

--- src/app/modules/country/containers/country-details/country-details.component.ts
+++ src/app/modules/country/containers/country-details/country-details.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin, map, Observable, of, switchMap } from 'rxjs';
+
+import { Country } from '../../models/country.model';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-country-details',
   templateUrl: './country-details.component.html',
-  styleUrls: ['./country-details.component.scss']
+  styleUrls: ['./country-details.component.scss'],
 })
 export class CountryDetailsComponent implements OnInit {
+  public country!: Country;
+  public borderCountries: string[] = [];
 
-  constructor() { }
+  constructor(
+    private countryService: CountryService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {
+  get currencies() {
+    return Object.keys(this.country.currencies);
   }
 
+  get languages() {
+    return Object.values(this.country.languages);
+  }
+
+  public ngOnInit(): void {
+    this.getCountryByName(this.route.snapshot.params['countryName'])
+      .pipe(
+        switchMap((country) => {
+          this.country = country;
+
+          if (!country?.borders?.length) return of([]);
+
+          const borderCountries = country.borders.map((border) =>
+            this.getCountryByName(border).pipe(
+              map((country) => country.name.common)
+            )
+          );
+
+          return forkJoin(borderCountries);
+        })
+      )
+      .subscribe((borderCountries) => {
+        this.borderCountries = borderCountries;
+      });
+  }
+
+  public getCountryByName(countryName: string) {
+    return this.countryService
+      .getCountryByName(countryName)
+      .pipe(map(([country]) => country));
+  }
 }
