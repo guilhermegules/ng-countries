90f2a6f2c4ae5548ffbda5559cf48986dd7494fa
Guilherme Gules Moreira <guilhermegules@gmail.com>
2022-05-03 22:36:53 -0300

    test: adding country service test

--- src/app/modules/country/services/country.service.spec.ts
+++ src/app/modules/country/services/country.service.spec.ts
@@ -1,10 +1,15 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { RegionEnum } from '../enums/region.enum';
 
 import { CountryService } from './country.service';
 
 describe('CountryService', () => {
   let service: CountryService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,5 +20,35 @@ describe('CountryService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get all countries', () => {
+    service.getAllCountries().subscribe();
+
+    const { request } = httpMock.expectOne(
+      'https://restcountries.com/v3.1/all'
+    );
+
+    expect(request.method).toBe('GET');
+  });
+
+  it('should get country by name', () => {
+    service.getCountryByName('brazil').subscribe();
+
+    const { request } = httpMock.expectOne(
+      'https://restcountries.com/v3.1/name/brazil'
+    );
+
+    expect(request.method).toBe('GET');
+  });
+
+  it('should get countries by continent', () => {
+    service.getCountriesByContinent(RegionEnum.AFRICA).subscribe();
+
+    const { request } = httpMock.expectOne(
+      'https://restcountries.com/v3.1/region/africa'
+    );
+
+    expect(request.method).toBe('GET');
   });
 });
