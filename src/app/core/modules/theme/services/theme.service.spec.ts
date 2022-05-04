0d2bd28e70430b7c5fcf7f8bab990df3872f26fb
Guilherme Gules Moreira <guilhermegules@gmail.com>
2022-05-03 22:36:40 -0300

    test: adding theme service tests

--- src/app/core/modules/theme/services/theme.service.spec.ts
+++ src/app/core/modules/theme/services/theme.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { ThemeEnum } from '../enums/theme.enum';
 
 import { ThemeService } from './theme.service';
 
@@ -12,5 +13,25 @@ describe('ThemeService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('setTheme', () => {
+    it('should throw an error when the passed theme is not valid', () => {
+      expect(service.setTheme('test' as any)).toThrowError(
+        '`Could not find theme with name test'
+      );
+    });
+    it('should set the light value on theme subject', (done) => {
+      service.setTheme(ThemeEnum.LIGHT);
+
+      service.theme$.subscribe((theme) => {
+        expect(theme).toEqual({
+          backgroundColor: '#ffffff',
+          color: '#111517',
+          themeKey: ThemeEnum.LIGHT,
+          bodyColor: '#fafafa',
+        });
+      });
+    });
   });
 });
